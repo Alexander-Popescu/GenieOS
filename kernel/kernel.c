79cc3b8ceff162ab8ccc249adbeebac6d2eb571e
Alexander-Popescu <AlexanderPopescu5381@gmail.com>
2024-07-18 13:20:25 -0400

    IO driver and TTY Char print

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,21 @@
-/* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
-void dummy_test_entrypoint() {
-}
+#include "../drivers/ports.h"
 
 int main() {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    //ask VGA control register (0x3d4) for high byte of cursor (14)
+    port_byte_out(0x3d4, 14);
+    //data is stored in data register (0x3d5)
+    int position = port_byte_in(0x3d5);
+    position = position << 8;
+
+    //now low byte (15)
+    port_byte_out(0x3d4, 15);
+    position += port_byte_in(0x3d5);
+
+    int vga_offset = position * 2; //vga stores another word for color info on each character slot
+
+    char *vga = (char *)0xB8000;
+    vga[vga_offset] = 'X';
+    vga[vga_offset + 1] = 0x0F; //color
+
+
 }
