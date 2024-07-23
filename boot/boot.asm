dd0ef38ad85340e7efe0500656a1ceabfee7dc83
Alexander-Popescu <AlexanderPopescu5381@gmail.com>
2024-07-22 21:27:25 -0400

    OS rewrite, real mode print

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,52 +1,17 @@
-[org 0x7C00]
-KERNEL_OFFSET equ 0x1000 ; load kernel after bootloader but before other memory maps
-
-	mov [BOOT_DRIVE], dl ; store in case dl is changed
-
-	mov bp, 0x9000 ; stack location
-	mov sp, bp
-
-	mov bx, REAL_MODE_MESSAGE
-	call printString
-	call printNewLine
-
-	call loadKernel
-	call switchProtected ; switch to 32 bit protected mode
-
-	jmp $
-
-%include "boot/disk.asm"
-%include "boot/printHex.asm"
-%include "boot/printString.asm"
-%include "boot/32bitPrint.asm"
-%include "boot/32bitGDT.asm"
-%include "boot/32bitSwitch.asm"
-
+[org 0x7c00]
 [bits 16]
-loadKernel:
-	mov bx, KERNEL_LOAD_MESSAGE
+
+	mov bx, .string
 	call printString
-	call printNewLine
 
-	mov bx, KERNEL_OFFSET ; put read data from disk into 0x1000
-	mov dh, 16
-	mov dl, [BOOT_DRIVE]
-	call diskLoad
-	ret
-
-	;time for our actual code
-[bits 32]
-protectedStart:
-	mov ebx, PROTECTED_MODE_MESSAGE
-	call printStringProtected
-	call KERNEL_OFFSET
 	jmp $
 
-REAL_MODE_MESSAGE db "Started in 16-bit real mode", 0
-PROTECTED_MODE_MESSAGE db "Switched to 32-bit protected mode", 0
-KERNEL_LOAD_MESSAGE db "Loading Kernel into memory", 0
-BOOT_DRIVE db 0 ;dl could be overwritten, so store it here
+%include "printString.asm"
 
-; identify sector as boot sector and pad sector
-times 510 - ($-$$) db 0
-dw 0xAA55
+.string:
+	db "Hello, World!"
+	db 0
+
+;boot sector padding
+times 510-($-$$) db 0
+dw 0xaa55
--- boot/printString.asm
+++ boot/printString.asm
@@ -1,34 +1,20 @@
-	;print function that starts printing a string
-	;at position specified by bx
+;assumes string to print begins at bx and ends with null byte
+
 printString:
-	push ax
-	push si
-	push bx
+	pusha
+	;write character video service
+	mov ah, 0x0e
+	
+	jmp .printStringLoop
+.printStringDone:
 
-	mov ah, 0x0E ; select bios function
-	mov si, bx
-
-.loop:
-	lodsb ; loads si into al then increments si
-	cmp al, 0 ; check for end of string
-	jz .done
-	int 0x10 ; print al to console
-	jmp .loop
-
-.done:
-	pop bx
-	pop si
-	pop ax
+	popa
 	ret
 
-printNewLine:
-    pusha
-    
-    mov ah, 0x0e
-    mov al, 0x0a ; newline char
-    int 0x10
-    mov al, 0x0d ; carriage return
-    int 0x10
-    
-    popa
-    ret
+.printStringLoop:
+	mov al, [bx]
+	cmp al, 0x0
+	je .printStringDone
+	int 0x10; print char
+	add bx, 1
+	jmp .printStringLoop
